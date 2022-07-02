7f414b2d76d348d1149bd8e721c53db4c5fc64cc
barthy <b.bonhomme@vucx.de>
2022-07-02 12:41:59 +0200

    madness

--- src/Preprocessor/Component.php
+++ src/Preprocessor/Component.php
@@ -2,29 +2,162 @@
 
 namespace TwigComponentTools\TCTBundle\Preprocessor;
 
+use SimpleXMLElement;
+
 class Component
 {
     public int $originalLength;
 
-    public bool $selfClosing;
+    public bool $isSelfClosing;
+
+    public bool $usesDefaultBlock;
 
     public int $originalEndPos;
 
     private array $blocks = [];
 
+    private ?string $inner = null;
+
+    private SimpleXMLElement $simpleXMLElement;
+
+    private mixed $simpleXMLBLocks;
+
+    /**
+     * @throws \Exception
+     */
     public function __construct(
-        public string $tag,
+        public string $openingTag,
         public string $name,
-        public string $attributes,
-        public int $originalStartPos
+        public string $filePath,
+        public int $originalStartPos,
+        string $code
     ) {
-        $this->originalLength = strlen($tag);
-        $this->originalEndPos = $originalStartPos + $this->originalLength;
-        $this->selfClosing = '/' === $tag[$this->originalLength - 2];
+        $startTagLength = strlen($openingTag);
+
+        $this->isSelfClosing  = '/' === $openingTag[$startTagLength - 2];
+        $this->originalEndPos = $this->originalStartPos + $startTagLength;
+
+        if (!$this->isSelfClosing) {
+            $closingTag        = "</$name>";
+            $endOfOpeningTag   = $this->originalStartPos + $startTagLength;
+            $startOfClosingTag = strpos($code, $closingTag, $endOfOpeningTag);
+
+            $this->originalEndPos = $startOfClosingTag + strlen($closingTag);
+            $this->inner          = substr($code, $endOfOpeningTag, $startOfClosingTag - $endOfOpeningTag);
+        }
+
+        $this->originalLength = $this->originalEndPos - $this->originalStartPos;
+        $fullComponentCode    = substr($code, $this->originalStartPos, $this->originalLength);
+
+        if ($this->isSelfClosing) {
+            $fullComponentCode = str_replace('/>', "></$name>", $fullComponentCode);
+        }
+
+        $this->simpleXMLElement = new SimpleXMLElement($fullComponentCode);
+        $this->simpleXMLBLocks  = $this->simpleXMLElement->xpath("/$name/block");
+        $this->usesDefaultBlock = empty($this->simpleXMLBLocks);
     }
 
     public function render(): string
     {
+        if ($this->isSelfClosing) {
+            return $this->renderInclude();
+        }
 
+        return $this->renderEmbed();
+    }
+
+    private function renderInclude(): string
+    {
+        $parameterMap = $this->getTwigParameterMap();
+
+        return "{% include '$this->filePath' with { props: { $parameterMap } } %}";
+    }
+
+    private function getTwigParameterMap(): string
+    {
+        $attributeObject = [];
+        $attributes      = $this->simpleXMLElement->attributes();
+
+        /**
+         * @var SimpleXMLElement $value
+         */
+        foreach ($attributes as $key => $value) {
+            $isVar = str_starts_with($value, '{{') && str_ends_with($value, '}}');
+
+            if ($isVar) {
+                $value = trim(substr($value, 2, -2));
+            }
+
+            $escape = $isVar ? '' : '\'';
+
+            if (!$isVar) {
+                $value = str_replace('\'', '\\\'', $value);
+            }
+
+            $attributeObject[] = "$key: $escape$value$escape";
+        }
+
+        return implode(', ', $attributeObject);
+    }
+
+    private function renderEmbed(): string
+    {
+        $parameterMap = $this->getTwigParameterMap();
+
+        $parts = array_filter([
+            $this->getAllBlockSets(),
+            "{% embed '$this->filePath' with { props: { $parameterMap } } %}",
+            $this->getAllBlockPrints(),
+            '{% endembed %}',
+        ]);
+
+        return join(PHP_EOL, $parts);
+    }
+
+    private function getAllBlockSets(): ?string
+    {
+        if ($this->usesDefaultBlock) {
+            return $this->getBlockSet('default', $this->inner);
+        }
+
+        if (!is_iterable($this->simpleXMLBLocks)) {
+            return null;
+        }
+
+        $parts = [];
+        foreach ($this->simpleXMLBLocks as $xmlBlock) {
+            $attributes = $xmlBlock->attributes();
+            $name       = $attributes['name'];
+
+            $childParts = [];
+            foreach ($xmlBlock->children() as $child) {
+                $childParts[] = $child->asXml();
+            }
+
+            $parts[] = $this->getBlockSet($name, join(PHP_EOL, $childParts));
+        }
+
+        return join(PHP_EOL, $parts);
+    }
+
+    private function getBlockSet(string $name, string $contents): string
+    {
+        $id = uniqid($name.'_');
+
+        $this->blocks[$name] = $id;
+
+        return "{%- set $id -%}$contents{%- endset -%}";
+    }
+
+    private function getAllBlockPrints(): ?string
+    {
+        $parts = [];
+
+        foreach ($this->blocks as $name => $id) {
+            $parts[] = "{%- block $name -%}{{- $id -}}{%- endblock -%}";
+        }
+
+        return join(PHP_EOL, $parts);
     }
 }
--- src/Preprocessor/ComponentPreprocessor.php
+++ src/Preprocessor/ComponentPreprocessor.php
@@ -2,9 +2,6 @@
 
 namespace TwigComponentTools\TCTBundle\Preprocessor;
 
-use Exception;
-use SimpleXMLElement;
-use Symfony\Component\VarDumper\VarDumper;
 use Twig\Source;
 use TwigComponentTools\TCTBundle\Naming\ComponentNamingInterface;
 
@@ -14,62 +11,37 @@ class ComponentPreprocessor implements PreprocessorInterface
 
     private string $componentRegex;
 
-    private array $blocks = [];
-
     public function __construct(ComponentNamingInterface $componentNaming)
     {
         $this->componentNaming = $componentNaming;
-        $this->componentRegex = $this->componentNaming->getComponentRegex();
+        $this->componentRegex  = $this->componentNaming->getComponentRegex();
     }
 
+    /**
+     * @throws \Exception
+     */
     public function processSourceContext(Source $source): Source
     {
-        $component = null;
-        $code = $source->getCode();
-        $lastPosition = 0;
+        /**
+         * @var ?\TwigComponentTools\TCTBundle\Preprocessor\Component $component
+         */
+        $component     = null;
+        $code          = $source->getCode();
+        $lastPosition  = 0;
         $hasComponents = false;
 
         while ($this->getNextComponent($code, $lastPosition, $component)) {
-            $this->blocks = [];
             $hasComponents = true;
 
-            [
-                'name' => $name,
-                'startPosition' => $startPosition,
-                // 'tag'           => $openingTag,
-                'endPosition' => $endOfOpeningTag,
-                'attributes' => $attributes,
-                'selfClosing' => $selfClosing,
-                'length' => $openingTagLength,
-            ] = $component;
-
-            if (!$selfClosing) {
-                $closingTag = "</$name>";
-                $startOfClosingTag = strpos($code, $closingTag, $endOfOpeningTag);
-
-                $code = $this->replaceClosingTag($code, $startOfClosingTag, $closingTag);
-                $code = $this->replaceDefaultBlock($code, $name, $endOfOpeningTag, $startOfClosingTag);
-            }
-
-            $code = $this->replaceOpeningTag(
+            $code = substr_replace(
                 $code,
-                $name,
-                $attributes,
-                $startPosition,
-                $openingTagLength,
-                $selfClosing
+                $component->render(),
+                $component->originalStartPos,
+                $component->originalLength
             );
-
-            $blocks = implode(PHP_EOL, $this->blocks);
-            $lastPosition = $startPosition + 1;
-            $code = substr_replace($code, $blocks, $startPosition, 0);
         }
 
         if ($hasComponents) {
-            $code = $this->replaceBlocks($code);
-
-            VarDumper::dump($code);
-
             return new Source($code, $source->getName(), $source->getPath());
         }
 
@@ -77,13 +49,15 @@ class ComponentPreprocessor implements PreprocessorInterface
     }
 
     /**
-     * @param string    $code         Code to search components in
-     * @param int       $lastPosition Offset at which to start searching for the next component
-     * @param Component $component    The component information will be saved in this array
+     * @param string     $code         Code to search components in
+     * @param int        $lastPosition Offset at which to start searching for the next component
+     * @param ?Component $component    The component information will be saved in this array
      *
      * @return bool True if it found a component
+     *
+     * @throws \Exception
      */
-    public function getNextComponent(string $code, int $lastPosition, Component &$component): bool
+    public function getNextComponent(string $code, int $lastPosition, ?Component &$component): bool
     {
         $matches = [];
 
@@ -99,115 +73,20 @@ class ComponentPreprocessor implements PreprocessorInterface
             return false;
         }
 
-        list($fullMatch, $nameMatch, $attributesMatch) = $matches;
-        list($tag, $start) = $fullMatch;
+        list($fullMatch, $nameMatch) = $matches;
+        list($openingTag, $start) = $fullMatch;
         list($name) = $nameMatch;
-        list($attributes) = $attributesMatch;
+
+        $path = $this->componentNaming->pathFromComponentName($name);
 
         $component = new Component(
-            $tag,
+            $openingTag,
             $name,
-            $attributes,
-            $start
+            $path,
+            $start,
+            $code
         );
 
         return true;
-    }
-
-    private function replaceClosingTag(string $code, int $closingPosition, string $closingTag): string
-    {
-        return substr_replace($code, '{% endembed %}', $closingPosition, strlen($closingTag));
-    }
-
-    private function replaceDefaultBlock(
-        string $code,
-        string $name,
-        int $endOfOpeningTag,
-        int $startOfClosingTag
-    ): string {
-        $inner = substr(
-            $code,
-            $endOfOpeningTag,
-            $startOfClosingTag - $endOfOpeningTag
-        );
-
-        $defaultBlock = !str_starts_with(trim($inner), '<block');
-
-        if ($defaultBlock) {
-            $id = uniqid($name).'_default';
-            $blockSet = "\n{% set {$id} %}$inner{% endset %}\n";
-
-            $this->blocks[] = $blockSet;
-
-            $code = substr_replace(
-                $code,
-                "\n{% block default %}{{- $id -}}{% endblock %}\n",
-                $endOfOpeningTag,
-                strlen($inner)
-            );
-        }
-
-        return $code;
-    }
-
-    private function replaceOpeningTag(
-        string $code,
-        string $name,
-        string $attributes,
-        int $startPosition,
-        int $tagLength,
-        bool $selfClosing
-    ): string {
-        $twigTag = $selfClosing ? 'include' : 'embed';
-        $path = $this->componentNaming->pathFromComponentName($name);
-        $params = $this->getTwigParameterMap($attributes);
-
-        return substr_replace(
-            $code,
-            "{% $twigTag '$path' with { props: { $params } } %}",
-            $startPosition,
-            $tagLength
-        );
-    }
-
-    private function getTwigParameterMap(string $attributesString): string
-    {
-        $attributeObject = [];
-        $attributesString = htmlspecialchars($attributesString, ENT_NOQUOTES);
-
-        try {
-            $element = new SimpleXMLElement("<element $attributesString />");
-            $attributes = $element->attributes();
-        } catch (Exception $exception) {
-            $attributes = [];
-        }
-
-        /**
-         * @var SimpleXMLElement $value
-         */
-        foreach ($attributes as $key => $value) {
-            $isVar = str_starts_with($value, '{{') && str_ends_with($value, '}}');
-
-            if ($isVar) {
-                $value = trim(substr($value, 2, -2));
-            }
-
-            $escape = $isVar ? '' : '\'';
-
-            if (!$isVar) {
-                $value = str_replace('\'', '\\\'', $value);
-            }
-
-            $attributeObject[] = "$key: $escape$value$escape";
-        }
-
-        return implode(', ', $attributeObject);
-    }
-
-    private function replaceBlocks(string $code): string
-    {
-        $code = preg_replace('/<block #([a-z]+)>/', '{% block $1 %}', $code);
-
-        return str_replace('</block>', '{% endblock %}', $code);
     }
 }
